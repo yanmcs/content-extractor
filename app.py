8f5d21acbd4f17049aabf3116ca42b30781588dd
Yan Menezes Carneiro da Silva <yamcs@me.com>
2022-06-09 18:47:16 -0300

    bug fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # MY scripts
 import content_extractor
 # Everything else
-from flask import Flask
+from flask import Flask, request
 from flask_restful import reqparse
 import os
 import json
@@ -11,7 +11,56 @@ app = Flask(__name__)
 browser = content_extractor.chrome_session(local=False)
 cfscrape_session = content_extractor.cfscrape_session
 
+@app.route('/teste')
+def teste():
+    if 'url' in request.args:
+        try:
+            url = request.args.get('url')  # set variable for url
+            # Get html from url
+            # Try using cfscrape first
+            html = content_extractor.extract_html_from_url(url, cfscrape_session())
+            print(html)
+            if not html:
+                # If cfscrape fails, use Chrome
+                html = content_extractor.extract_html_from_url(url, browser)
+                print(html)
+            # Parse html to json
+            result = content_extractor.html_to_json(html)
+        except Exception as e:
+            return str(e), 500, {'Content-Type': 'text/plain; charset=utf-8'}
+        else:
+            # return article text
+            if request.args.get('format') == 'json':
+                return json.dumps(result['article_content']), 200, {'Content-Type': 'application/json'}
+            elif request.args.get('format') == 'text':
+                return str(result['article_text']), 200, {'Content-Type': 'text/plain; charset=utf-8'}
+            elif request.args.get('format') == 'html':
+                return str(result['article_html_content']), 200, {'Content-Type': 'text/html; charset=utf-8'}
+            elif request.args.get('format') == 'links':
+                return json.dumps(result['urls']), 200, {'Content-Type': 'application/json'}
+            else:
+                return json.dumps(result), 200, {'Content-Type': 'application/json'}
+    else:
+        # Set html form
+        form_html =  '''
+        <form method="post" action="/article-extractor">
 
+            <p>Enter URL for article text:</p>
+            <input type="text" name="url">
+
+            <select name="format">
+                <option value="html">HTML</option>
+                <option value="text">Text</option>
+                <option value="json">JSON</option>
+                <option value="links">Links</option>
+            </select>
+
+            <input type="submit" value="Submit">
+            <input type="reset" value="Reset">
+        </form>
+        '''
+        return form_html, 200, {'Content-Type': 'text/html; charset=utf-8'}
+        
 @app.route("/")
 def form():
     # Set html form
